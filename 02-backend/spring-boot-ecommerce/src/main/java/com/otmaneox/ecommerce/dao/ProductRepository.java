316cb37fe0513c3691283cb5c030e82ce6ac67b3
Otmaneox <arsalane.otmane@gmail.com>
2024-01-13 15:00:31 +0100

    first commit

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/otmaneox/ecommerce/dao/ProductRepository.java
@@ -0,0 +1,19 @@
+package com.otmaneox.ecommerce.dao;
+
+import com.otmaneox.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@Repository
+@CrossOrigin("http://localhost:4200")
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    //same as SELECT * FROM product where category_id=?
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
+    Page<Product> findByNameContaining(@Param("name") String name, Pageable pageable);
+
+
+}
